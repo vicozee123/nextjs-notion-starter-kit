fc6a10871708f5a76542d897bcaa7bccb0980754
vicoz <vicozee97@163.com>
2025-05-12 15:43:51 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '613ee3afda7b406c9cac7ba2965502cc',
+  rootNotionPageId: '1f11854ab74e80c29534c24ce316579a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Chaodi Tang Blog World',
-  domain: 'chaodit.com',
-  author: 'Chaodi Tang',
+  name: 'Vicoz',
+  domain: 'vicoz.com',
+  author: 'vicoz zeng',
 
   // open graph metadata (optional)
-  description: 'My Blogs.',
+  description: 'Vicoz's Blogs',
 
   // social usernames (optional)
   twitter: 'chaodit',
